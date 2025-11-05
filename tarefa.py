c481041507af1219ef7a5c6dc25fe92ae32b359a
AndreVitorChaves <andrevitorchaves.f@gmail.com>
2025-11-04 21:26:22 -0300

    Adicionando Banco de Dados

--- tarefa.py
+++ tarefa.py
@@ -1,28 +1,37 @@
 from flask import jsonify
+from conexao import get_conexao
+from psycopg2.extras import RealDictCursor
 
 def buscar_tarefas():
-    tarefas = [
-        {
-            'id': 1,
-            'nome': 'Aprender digitacao',
-            'descricao': 'Vamos aumentar o zoom para nao errar a digitacao',
-            'status': 'Pendente'
-        },
-        
-        {
-            'id': 2,
-            'nome': 'Aprender Python',
-            'descricao': 'Aprender Python para programar apis',
-            'status': 'Pendente'
-        }
-    ]
-    return jsonify(tarefas)
+    #conecta no banco de dados
+    con = get_conexao()
+    cursor = con.cursor(cursor_factory=RealDictCursor)
+    cursor.execute(
+        "SELECT id, name, description FROM todos;"
+    )
 
-def buscar_tarefa():
-    tarefa = {
-        'id': 1,
-            'nome': 'Aprender digitacao',
-            'descricao': 'Vamos aumentar o zoom para nao errar a digitacao',
-            'status': 'Pendente'
-    }
-    return jsonify(tarefa)
+    #busca os dados e armazena na variável
+    todos = cursor.fetchall()
+
+    #fecha as conexões
+    cursor.close()
+    con.close()
+
+    return jsonify(todos)
+
+def buscar_tarefa(id):
+    #conecta no banco de dados
+    con = get_conexao()
+    cursor = con.cursor(cursor_factory=RealDictCursor)
+    cursor.execute(
+        "SELECT id, name, description FROM todos WHERE id = %s", (id,)
+    )
+
+    #busca os dados e armazena na variável
+    todo = cursor.fetchone()
+
+    #fecha as conexões
+    cursor.close()
+    con.close()
+
+    return jsonify(todo)
